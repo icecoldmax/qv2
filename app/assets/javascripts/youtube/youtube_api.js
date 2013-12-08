208fe5f2bf9ab59cd8b7eb538f49ea63099bbc9a
David Nettle <david.nettle@gmail.com>
2013-12-08 22:55:23 +1100

    Remove unnecessary youtube api script file calls. Also experiment with https origin parameter.

--- app/assets/javascripts/youtube/youtube_api.js
+++ app/assets/javascripts/youtube/youtube_api.js
@@ -4,7 +4,7 @@ function onYouTubePlayerAPIReady() {
         height: '480px',
         playerVars: { 'autoplay': 0, 'controls': 0, 'wmode': 'opaque',
             //'playlist': vids.join(),
-            'origin': 'http://localhost:3000/',
+            'origin': 'https://quizvids.herokuapp.com/'
             'modestbranding': 1, 'showinfo': 0 
         },
         events: {
