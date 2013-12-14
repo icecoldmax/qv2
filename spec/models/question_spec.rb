2f8420ab11e81609b40e8b984a27acbd32863dd2
David Nettle <david.nettle@gmail.com>
2013-12-14 22:36:23 +1100

    Include spec for custom validation.

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -5,4 +5,25 @@ describe Question do
   it { should have_many(:answers) }
 
   it { should validate_presence_of :content }
+
+  describe "#one_correct_answer" do
+    let(:user) { FactoryGirl.create(:user) }
+    let(:quiz) { Quiz.new(name: "Quiz", user: user, questions_attributes: { "0" => { content: "first q", answers_attributes: answers_attributes } } ) }
+
+    context "too many correct answers" do 
+      let(:answers_attributes) { { "0" => { content: "a1", correct: true }, "1" => { content: "a2", correct: true }, "2" => { content: "a3", correct: true }, "3" => { content: "a4", correct: true } } }
+
+      it "should not be valid" do
+        expect(quiz).to_not be_valid
+      end
+    end
+
+    context "just one correct answer" do
+      let(:answers_attributes) { { "0" => { content: "a1", correct: false }, "1" => { content: "a2", correct: false }, "2" => { content: "a3", correct: false }, "3" => { content: "a4", correct: true } } }
+
+      it "should be valid" do
+        expect(quiz).to be_valid
+      end
+    end
+  end
 end
